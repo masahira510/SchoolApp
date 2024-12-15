c92279d62f39b6997a9d332424d8da5da9c14bcd
masahira510 <eric13thfriday30kikimama@gmail.com>
2024-12-15 23:55:14 +0900

    Main.java更新しました

--- SchoolApp/src/Main.java
+++ SchoolApp/src/Main.java
@@ -6,32 +6,20 @@ public class Main {
     public static void main(String[] args) {
         SchoolService Service = new SchoolService();
 
-        // 生徒
-        Student s1 = new Student("ドウェイン・ジョンソン",14,"男");
-        Student s2 = new Student("ジェイソン・ステイサム",10,"女");
-        Student s3 = new Student("ヴィン・ディーゼル",15,"男");
-        Student s4 = new Student("ブルース・ウィルス",9,"男");
-
-        // 教師
-        Teacher t1 = new Teacher("武田信玄",35,"女");
-        Teacher t2 = new Teacher("徳川家康",40,"女");
-        Teacher t3 = new Teacher("織田信長",40,"男");
-        Teacher t4 = new Teacher("伊達政宗",9,"男");
-
         // クラスの配列宣言
         Student[] studentList = new Student[4];
         Teacher[] teacherList = new Teacher[4];
 
         // 各要素にオブジェクトを代入
-        studentList[0] = s1;
-        studentList[1] = s2;
-        studentList[2] = s3;
-        studentList[3] = s4;
+        studentList[0] = new Student("ドウェイン・ジョンソン",14,"男");
+        studentList[1] = new Student("ジェイソン・ステイサム",10,"女");
+        studentList[2] = new Student("ヴィン・ディーゼル",15,"男");
+        studentList[3] = new Student("ブルース・ウィルス",9,"男");
 
-        teacherList[0] = t1;
-        teacherList[1] = t2;
-        teacherList[2] = t3;
-        teacherList[3] = t4;
+        teacherList[0] = new Teacher("武田信玄",35,"女");
+        teacherList[1] = new Teacher("徳川家康",40,"女");
+        teacherList[2] = new Teacher("織田信長",40,"男");
+        teacherList[3] = new Teacher("伊達政宗",9,"男");
         
         // forでループ処理し、リスト追加メソッドに値を追加
         for(Student stu : studentList) {
